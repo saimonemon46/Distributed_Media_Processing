d69957b100a52bdace681f2d58aab2cb78fe33ba
Md. Saimon Hasan Emon <saimonemon46@gmail.com>
2026-07-24 01:14:53 +0600

    Multipart file uploads, resize images, save result and return result

--- main.py
+++ main.py
@@ -1,7 +1,48 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from PIL import Image  
+
+import io
+import os
+import uuid
 
 app = FastAPI()
+
+
+OUTPUT_DIR = "output"
+
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+
 
 @app.get("/health")
 def health():
     return {"Status" : "Healthy"}
+
+
+@app.post("/jobs")
+def create_job(file : UploadFile = File(...)):
+    # 1. Read Uploaded Files
+    contents = file.file.read()
+    
+    # 2. Convert Bytes into image
+    image = Image.open(io.BytesIO(contents))  # Open image with pillow
+    
+    # 3. Resize image
+    image.thumbnail((1000,200))
+    
+    # 4. Generate Unique File name
+    filename = f"{uuid.uuid4()}.jpg"
+
+    output_path = os.path.join(OUTPUT_DIR, filename)
+    
+    # 5. Convert RGB and Save
+    image.convert("RGB").save(output_path)
+    
+    # 6. Return Result
+    return {
+        "status": "completed",
+        "original_filename": file.filename,
+        "output_file": output_path,
+        "width": image.width,
+        "height": image.height,
+    }
